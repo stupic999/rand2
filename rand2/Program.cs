7df8e2750c32e7d29ddf821c6474a74c87dae7bb
m305-13\w401 <aierzhiguang2@gmail.com>
2018-04-12 17:36:34 +0800

    作业完成啦

--- rand2/Program.cs
+++ rand2/Program.cs
@@ -11,6 +11,18 @@ namespace rand2
         static void Main(string[] args)
         {
             Random rand = new Random();
+            List<int> number = new List<int>();
+            for (int i = 0; i < 42; i++)
+            {
+                number.Add(i);
+            }
+            for (int i = 0; i <6; i++)
+            {
+                int j = rand.Next(1,number.Count);
+                Console.Write(number[j]+" ");
+                number.RemoveAt(j);
+            }
+            Console.ReadLine();
         }
     }
 }
